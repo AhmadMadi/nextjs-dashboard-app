3830d786aa36ac56c6a3ab984d7bfc5d382e47fa
AhmadMadi <madi96.at.work@gmail.com>
2024-09-24 21:07:55 +0300

    Add input validation using Zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,16 +1,31 @@
 'use server';
 
+import { z } from 'zod';
+
+const InvoiceFormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['paid', 'pending']),
+    date: z.string(),
+});
+
+const CreateInvoice = InvoiceFormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
+    });
 
     /**
      * OR you can do this:
-     * const rawFormData = Object.fromEntries(formData.entries());
+     * const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
      */
 
-    console.table(rawFormData);
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    console.table({ customerId, amount, status });
 }
