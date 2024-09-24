01344db93be37fd5d073e910df13f490730986cf
AhmadMadi <madi96.at.work@gmail.com>
2024-09-24 21:12:13 +0300

    Revalidate invoice list and redirect to invoice list after invoice insertion into PSQL through server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const InvoiceFormSchema = z.object({
     id: z.string(),
@@ -32,4 +34,7 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
